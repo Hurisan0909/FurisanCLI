331dafead6783587af8ea708f42082a7567266f3
Hurisan <160610196+Hurisan0909@users.noreply.github.com>
2025-09-05 22:26:53 +0900

    Add files via upload

---
+++ CLI/Renko/Renko.py
@@ -0,0 +1,8 @@
+print("=====連呼プログラム=====")
+print("[[©2025 FuriCLI All Rights Reserved]]")
+print("### Ver.1.0 ###")
+world_data = input("何を連呼する？ >> ")
+count_data = int(input("何回連呼する？ >> "))
+
+for i in range(count_data):
+    print(world_data)
